05741caa75066a70d662804018900dd17f95a907
Stephen Sullivan <sullivankstephen@gmail.com>
2015-05-02 15:42:11 -0500

    setting up basic api

---
+++ argon-backend/src/main/java/com/sks/ArgonBackend/ARComponent.java
@@ -0,0 +1,9 @@
+package com.sks.ArgonBackend;
+
+/**
+ * Class to hold a data pertaining to a flowchart component.
+ */
+public class ARComponent {
+    public String displayName;
+    public String componentType;
+}
